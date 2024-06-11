2ae2d06ede90cfecec077f01a085c5d1668eb39a
Shawn <shawn_aberin@dlsu.edu.ph>
2024-06-12 00:20:26 +0800

    main should have a window now

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,18 @@
 int main(int argc, char const *argv[])
 {
     /* code */
+    GLFWwindow* window;
+    if (!glfwInit() )
+    return -1;
+    else window = glfwCreateWindow(1280,720,"DISCM SIM",NULL,NULL);
     Program frag = Program("foo","bar");
+    if(!window){ glfwTerminate(); return -1;}
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        
+    }
+    
     return 0;
 }
