81b71a64e7ef41c0e4d6da81ed2ec2ac87f2563e
tamadNaBrowNie <shawn_aberin@dlsu.edu.ph>
2024-06-20 17:53:07 +0800

    have speed and theta insert mode reday

--- Main.cpp
+++ Main.cpp
@@ -3,12 +3,16 @@
 #include "Point.h"
 #include <vector>
 #include <iostream>
+#include <chrono>
+#include <future>
+
 // Keybinds for actions
 enum Acts
 {
-    KILL = GLFW_KEY_ENTER,
-    DATA = GLFW_KEY_ESCAPE,
-    ADD = GLFW_KEY_SPACE
+    KILL =  GLFW_KEY_ESCAPE,
+    dPOS = GLFW_KEY_1,
+    dSPEED = GLFW_KEY_2,
+    dTHETA = GLFW_KEY_3
 };
 // Screen
 constexpr float X_MAX = 1280.f;
@@ -16,38 +20,82 @@ constexpr float Y_MAX = 720.f;
 
 void Key_Callback(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
+    // TODO: MUTEX THIS
+    std::vector<Point*>* arr = (std::vector<Point*> *)glfwGetWindowUserPointer(window);
+
     float x_pos = 0, y_pos = 0, s = 0, deg = 0;
+
+    auto hl = [&](int T, int n){
+        
+        float start = 0 ,end = 0;
+
+        
+        auto push = [&](float* f) {
+
+            std::cin >> x_pos;
+            std::cin >> y_pos;
+            float d = (end - start) / (float)n;
+            *f = start;
+            for (int i = 0; i < n; i++) {
+
+                arr->push_back(new Point(x_pos, y_pos, s, deg));
+                *f += d;
+            }
+
+            };
+        switch (T) {
+        case Acts::dPOS:
+            break;
+        case Acts::dSPEED:
+            std::cin >> deg;
+            std::cin >> start;
+            std::cin >> end;
+     
+           
+            
+            push(&s);
+            break;
+
+        case Acts::dTHETA:
+            std::cin >> start;
+            std::cin >> end;
+            std::cin >> s;
+           
+            push(&deg);
+            break;
+        default: std::cerr << "HOW TF";
+        }
+        
+        
+    };
     
-        // TODO: MUTEX THIS
-  /*std::vector<Point>* arr = (std::vector<Point> *)glfwGetWindowUserPointer(window);
-  */
         switch (key)
         {
-        case Acts::DATA:
-
-            std::cout << "waitng?";
-            std::cin.get();
-            std::cout << "waitng";
-            // TODO: HANDLE BALL INPUT
+        case Acts::dPOS:
+            
+        case Acts::dSPEED:
+          
+        case Acts::dTHETA:
+            std::cout << "n = ";
+            std::async(hl, key,std::cin.get());
             break;
+           
         case Acts::KILL:
             // KILL WINDOW
             glfwSetWindowShouldClose(window, true);
             break;
-        case Acts::ADD:
-            // CREATES INSTANCE OF BALLS
-            std::cout << "I was told not to make the joke";
-            break;
+
         default:;
         }
     
 }
+
 int main(int argc, char const *argv[])
 {
     /* code */
     float vertices[] = {
         // positions
-        0.f, 0.f
+        10.f, -10.f
         };
     GLFWwindow *window = nullptr;
     GLenum init = glfwInit();
@@ -91,16 +139,26 @@ int main(int argc, char const *argv[])
     glLoadIdentity();
     glOrtho(0.0f, X_MAX, Y_MAX, 0.0f, 0.f, 1.f);
     int width  =0, height =0;
-    //glCullFace(GL_BACK);
     glEnable(GL_PROGRAM_POINT_SIZE);
-    glPointSize(5.f);
+    glPointSize(10.f);
+    constexpr double TIC = 16.6666666667;
+
+    double last_tic = glfwGetTime() * 1000.f, new_tic = glfwGetTime() * 1000.f,dt =0;
+    std::vector<Point*> arr;
+    glfwSetWindowUserPointer(window, &arr);
     while (!glfwWindowShouldClose(window))
     {
+        new_tic = glfwGetTime() * 1000.f;
+        dt = new_tic - last_tic;
         glfwGetWindowSize(window,&width, &height);
         glad_glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
         // glDrawArraysInstanced(GL); //TODO: try to make it instanced
         prog.use();
+        if (TIC <= dt)
+        {   
+            //PUT BOING HERE
+        }
         glBindVertexArray(VAO);
         glDrawArrays(GL_POINTS, 0, 2);
         glfwSwapBuffers(window);
